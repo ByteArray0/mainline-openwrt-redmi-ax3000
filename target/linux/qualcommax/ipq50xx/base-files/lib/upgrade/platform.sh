4b73cbfdf5b8e01c7615b741dcaa429a53b2a334
ByteArray0 <134892304+ByteArray0@users.noreply.github.com>
2026-04-14 23:53:01 +0800

    device: add support for cmcc rax3000q

--- target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -195,6 +195,9 @@ platform_do_upgrade() {
 		remove_oem_ubi_volume wifi_fw
 		nand_do_upgrade "$1"
 		;;
+	cmcc,rax3000q)
+		nand_do_upgrade "$1"
+		;;
 	glinet,gl-b3000)
 		glinet_do_upgrade "$1"
 		;;
